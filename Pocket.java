40e5eb05c18ba342d80d4becb4d2422361cab5d3
FridaArroyo <78165645+FridaArroyo@users.noreply.github.com>
2022-04-06 21:23:29 -0500

    Add files via upload

---
+++ Pocket.java
@@ -0,0 +1,56 @@
+package pocket;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Pocket {
+    public static void main(String[] args) {
+       
+        Deck deck= new Deck();
+        deck.construir();
+        Scanner scanner = new Scanner(System.in);
+        boolean salir = false;
+        int opciones;
+        System.out.println("Bienvenido a Poker!");
+
+        while (!salir){
+            System.out.println("Opciones a elegir:");
+            System.out.println("1 Mezclar deck");
+            System.out.println("2 Sacar una carta");
+            System.out.println("3 Carta al azar");
+            System.out.println("4 Generar una mano de 5 cartas");
+            System.out.println("0 Salir");
+            try {
+                System.out.println("Selecciona una opción:");
+                opciones = scanner.nextInt();
+                switch (opciones){
+                    case 1:
+                        deck.barajear();
+                        break;
+                    case 2:
+                        deck.head();
+                        break;
+                    case 3:
+                        deck.pick();
+                        break;
+                    case 4:
+                        deck.hand();
+                        break;
+                    case 0:
+                        salir = true;
+                        System.out.println("Se acabo el juego");
+                        break;
+                    default:
+                        throw new IllegalStateException("opción no valida " + opciones);
+                }
+            }
+            catch (InputMismatchException e){
+                System.out.println("Opción no válida");
+                scanner.next();
+            }
+            catch (Exception a){
+                a.printStackTrace();
+            }
+        }
+        
+    }
+    
+}
